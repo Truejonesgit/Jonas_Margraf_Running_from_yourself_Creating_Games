ffe186db816fd8ad553cffde2108236393003b0a
Truejonesgit <57531141+Truejonesgit@users.noreply.github.com>
2019-11-26 20:24:01 +0100

    Revert "Revert "updated player movement""
    This reverts commit 911be7109c78cb378b381fb5d8da210fba27c19e.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,36 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-
     public float moveSpeed;
+    //private Rigidbody theRB;
     public float jumpForce;
     public CharacterController controller;
     private Vector3 moveDirection;
     public float gravityScale;
     public float rotateSpeed;
     public GameObject playerModel;
-    Animator animator;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //theRB = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
 
+        /*if (Input.GetButtonDown("Jump"))
+
+        {
+
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+
+
+        }
+        */
 
         moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
 
-
+        
         if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
         {
             Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
@@ -53,10 +61,6 @@ public class ZombieController : MonoBehaviour
 
         moveDirection.y = moveDirection.y + (Physics.gravity.y * Time.deltaTime * gravityScale);
         controller.Move(moveDirection * Time.deltaTime);
-
-
-
-
 
 
 
